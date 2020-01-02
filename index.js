217d4dff46e8dce4a9576b40f01a5fdfc8dd3b91
FACUNDO ROCHA <facundo@estudiorochayasoc.com.ar>
2020-01-02 18:17:00 -0300

    hola

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.listen(process.env.PORT);
-
+console.log(process.env);
 //Routes
 app.use('/mercadolibre', require("./routes/MercadolibreRoute"));
 app.use('/product', require("./routes/ProductRoute"));
